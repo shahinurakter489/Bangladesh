7dd1c3e9485dad61fac857a1bee79acacc55356e
shahin <shahinurakter085bd@gmail.com>
2021-06-17 22:49:49 -0400

    This is my Bangladesh project.

---
+++ src/test/java/myBangladeshPackage/BangladeshClass.java
@@ -0,0 +1,22 @@
+package myBangladeshPackage;
+
+public class BangladeshClass {
+
+	public static void main(String[] args) {
+		for (char c='A';c<='Z';c++) {
+			System.out.println(c+" ");
+			
+			int a= 5;
+			int mult;
+			for (int i=1; i<=10;i++) {
+				mult=a*i;
+				System.out.println(a+"x"+i+"="+mult);
+			}
+			
+			
+			
+		}
+
+	}
+
+}
